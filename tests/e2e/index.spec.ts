cb5f50f9b159bd18f8e23133fa054da1f5b709e2
Florent A. <fa+git@hex46.fr>
2024-11-01 09:28:09 +0100

    Add Playwright test

--- tests/e2e/index.spec.ts
+++ tests/e2e/index.spec.ts
@@ -1,7 +1,12 @@
 import { test, expect } from "@playwright/test";
 
-test("title is correct", async ({ page }) => {
+test("title component is present", async ({ page }) => {
   await page.goto("http://localhost:4321/");
 
-  await expect(page).toHaveTitle("Home");
+  await expect(
+    page.getByRole("heading", {
+      name: "The Infrared Face of the Andromeda Galaxy",
+      level: 2,
+    }),
+  ).toBeVisible();
 });
