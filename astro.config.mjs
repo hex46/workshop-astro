76d1798e65dfbfcd9f07e82b2ef91b35e66c44c1
Jeremy Chauvin <jeremychauvin10@gmail.com>
2024-11-21 08:08:24 +0100

    :fire: delete file

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,25 +3,12 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
-
-import node from "@astrojs/node";
-
 // https://astro.build/config
 export default defineConfig({
   // 'server or 'hybrid'
-  output: "hybrid",
-
-  prefetch: true,
-
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
-    react(),
   ],
-
-  adapter: node({
-    mode: "standalone",
-  }),
 });
