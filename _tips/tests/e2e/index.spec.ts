54bafeabb12c9ade050aeaea7d79e12f341fdb41
Florent A. <fa+git@hex46.fr>
2024-12-08 18:35:34 +0100

    :bulb: Add tips

--- _tips/tests/e2e/index.spec.ts
+++ _tips/tests/e2e/index.spec.ts
@@ -1,14 +1,14 @@
 import { test, expect } from "@playwright/test";
 
-test.skip("title component is present", async ({ page }) => {
+test("title is present", async ({ page }) => {
   // Go to home
   await page.goto("http://localhost:4321/");
 
-  // Test <h2>
+  // Test <h1>
   await expect(
     page.getByRole("heading", {
-      name: "The Infrared Face of the Andromeda Galaxy",
-      level: 2,
+      name: "Hello there!",
+      level: 1,
     }),
   ).toBeVisible();
 });
--- _tips/tests/e2e/today.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test.skip("Today page", async ({ page }) => {
-  // Go to home
-  await page.goto("/today");
-
-  // Test text
-  await expect(page.getByText("Source")).toBeVisible();
-});
