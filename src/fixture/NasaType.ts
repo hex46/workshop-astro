ad8eb3102738d2cf7550541c0f2271d413eb4478
Jeremy Chauvin <jeremychauvin10@gmail.com>
2025-01-12 10:45:33 +0100

    :sparkles: ajout de l'etape 7

---
+++ src/fixture/NasaType.ts
@@ -0,0 +1,41 @@
+export type NasaCollection = {
+  collection: {
+    version: string;
+    href: string;
+    items: NasaItem[];
+    metadata: CollectionMetadata;
+    links?: PaginationLink[];
+  };
+};
+export type NasaItem = {
+  href: string;
+  data: NasaData[];
+  links: ResourceLink[];
+};
+type NasaData = {
+  center: string;
+  title: string;
+  nasa_id: string;
+  date_created: string;
+  media_type: "image" | "video";
+  description?: string;
+  description_508?: string;
+  keywords?: string[];
+  secondary_creator?: string;
+  album?: string[];
+  location?: string;
+  photographer?: string;
+};
+type ResourceLink = {
+  href: string;
+  rel: string;
+  render?: string;
+};
+type CollectionMetadata = {
+  total_hits: number;
+};
+type PaginationLink = {
+  rel: string;
+  prompt: string;
+  href: string;
+};
---
+++ src/tests/e2e/step7/step7.spec.ts
@@ -0,0 +1,37 @@
+import { test } from "playwright/test";
+import { expect } from "@playwright/test";
+
+test.describe("Etape 7 - Creer un formulaire de recherche et des cartes", () => {
+  test("Afficher le formulaire SANS ACTION", async ({ page }) => {
+    await page.goto("/search");
+
+    // Ajout de la balise form
+    const form = page.getByRole("form", {
+      name: "Search",
+    });
+    await expect(form).toBeVisible();
+
+    // Ajout du text input
+    const queryTextBox = form.getByRole("textbox", { name: "query" });
+    await expect(queryTextBox).toBeVisible();
+
+    // Ajout du bouton de submit
+    const submitButton = form.getByRole("button", { name: "Search" });
+    await expect(submitButton).toBeVisible();
+  });
+
+  test("On rempli le formulaire et on recherche la Milky Way. On attend une card en sortie", async ({
+    page,
+  }) => {
+    await page.goto("/search");
+
+    const form = page.getByRole("form", {
+      name: "Search",
+    });
+    await form.getByRole("textbox", { name: "query" }).fill("Milky Way");
+    await form.getByRole("button", { name: "Search" }).click();
+
+    const cards = page.getByTestId("card");
+    expect(cards.count()).toBe(1);
+  });
+});
