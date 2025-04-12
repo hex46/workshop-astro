bc2142fbfae6dc5b5ed806f25d2b95c8cfb981a4
Florent A. <fa+git@hex46.fr>
2025-04-12 20:12:47 +0200

    :fire: Remove code

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,16 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+/*
 import node from "@astrojs/node";
+*/
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  /*output: "server",*/
+  output: "static",
   integrations: [react()],
-  adapter: node({
+  /*adapter: node({
     mode: "standalone",
-  }),
+  }),*/
 });
